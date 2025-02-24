ead9d18b558b2f96e89062b2a01b1e3a62f9d649
Dario Airoldi <darioa@microsoft.com>
2025-02-24 13:13:13 -0800

    Refactor Azure components and update package references
    - Removed `Class1.cs` and its namespace declaration.
    - Added `<AzureCosmosDisableNewtonsoftJsonCheck>` property in `Diginsight.Components.Azure.csproj`.
    - Reformatted image file reference and added `Microsoft.Azure.Cosmos` package.
    - Updated multiple package references in `Diginsight.Components.csproj` to use wildcard versions for flexibility.
    - Created `CosmosDbExtensions.cs` with a static class for Azure Cosmos DB extension methods.

---
+++ src/Diginsight.Components.Azure/Extensions/CosmosDbExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diginsight.Components.Azure;
+
+public static class CosmosDbExtensions
+{
+    //public FeedIterator<T> GetItemQueryIterator<T>(this Container Container, QueryDefinition queryDefinition, string continuationToken = null, QueryRequestOptions requestOptions = null) { 
+    
+    //}
+}

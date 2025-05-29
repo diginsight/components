60bb8c98eb5e0b7a423c7f43e89e9e0dae4a021b
Dario Airoldi <darioa@microsoft.com>
2025-05-29 19:12:21 +0200

    ParallelExtensions and deploy fix

--- src/Diginsight.Components/Extensions/ParallelExtensions.cs
+++ src/Diginsight.Components/Extensions/ParallelExtensions.cs
@@ -1,26 +1,64 @@
 ﻿#region using
 #endregion
 
+using Diginsight.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
 namespace Diginsight.Components;
 
 public static class ParallelExtensions
 {
-    public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> body, int maxDegreeOfParallelism = 4)
+    public static async Task<IEnumerable<R>> ForEachAsync<T, R>(this IEnumerable<T> source, Func<T, Task<R>> body, ParallelOptions options)
     {
-        var tasks = new List<Task>();
-        using SemaphoreSlim semaphore = new(maxDegreeOfParallelism);
-        foreach (var item in source)
+        var logger = Observability.LoggerFactory.CreateLogger(typeof(ParallelExtensions));
+        using var activity = Observability.ActivitySource.StartMethodActivity(logger, new { body, options });
+
+        var tasks = new List<Task<R>>();
+
+        int maxDegreeOfParallelism = options.MaxDegreeOfParallelism > 0 ? options.MaxDegreeOfParallelism : Environment.ProcessorCount;
+        using (var semaphore = new SemaphoreSlim(maxDegreeOfParallelism))
         {
-            await semaphore.WaitAsync();
-            try
+            foreach (var item in source)
             {
-                tasks.Add(body(item));
-            }
-            finally
-            {
-                semaphore.Release();
+                await semaphore.WaitAsync();
+                try
+                {
+                    tasks.Add(body(item));
+                }
+                finally { semaphore.Release(); }
             }
         }
-        await Task.WhenAll(tasks);
+        var result = await Task.WhenAll(tasks);
+        activity?.SetOutput(result);
+        return result;
     }
+
+    public static async Task<IEnumerable<T>> WhenAllAsync<T>(this IEnumerable<Func<Task<T>>> asyncFuncs, ParallelOptions parallelOptions = null)
+    {
+        var logger = Observability.LoggerFactory.CreateLogger(typeof(ParallelExtensions));
+        using var activity = Observability.ActivitySource.StartMethodActivity(logger, new { asyncFuncs, parallelOptions });
+
+        if (asyncFuncs == null) throw new ArgumentNullException(nameof(asyncFuncs));
+        parallelOptions ??= new ParallelOptions();
+
+        int index = 0;
+        var resultsDictionary = new ConcurrentDictionary<int, T>();
+        var indexedFuncs = asyncFuncs.Select(func => (Index: Interlocked.Increment(ref index) - 1, Func: func)).ToList();
+        
+
+        await Parallel.ForEachAsync(indexedFuncs, parallelOptions, async (item, cancellationToken) =>
+        {
+            var result = await item.Func();
+            resultsDictionary[item.Index] = result;
+        });
+
+        var result = indexedFuncs.OrderBy(x => x.Index).Select(x => resultsDictionary[x.Index]);
+
+        activity?.SetOutput(result);
+        return result;
+    }
+
+
 }

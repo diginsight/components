b7cb255bdf8bc59ea5caf74f40224e863daea83b
Dario Airoldi <darioa@microsoft.com>
2025-06-06 07:08:26 -0700

    .

---
+++ src/Diginsight.Components/Service/ParallelService.cs
@@ -0,0 +1,14 @@
+﻿namespace Diginsight.Components
+{
+    public class ParallelService
+    {
+        // get httpClient
+        public ParallelService() { }
+
+        // Get // trace outgoing calls
+        // Post
+        // Put
+        // Path
+        // Delete
+    }
+}

0205ce1e48ae9f663bf484756d715b09761af21a
Dario Airoldi <darioa@microsoft.com>
2025-07-07 23:49:20 -0700

    AzureTableRepository<T> fix

---
+++ src/Diginsight.Components.Azure/Observability.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diginsight.Components.Azure;
+
+internal static class Observability
+{
+    public static readonly ActivitySource ActivitySource = new(Assembly.GetExecutingAssembly().GetName().Name!);
+
+    internal static ILoggerFactory LoggerFactory = default!;
+
+
+}
+

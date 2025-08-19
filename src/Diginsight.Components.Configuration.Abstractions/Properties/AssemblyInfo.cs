5d3516611bb44feaee69cc4b5653a33706c31369
Dario Airoldi <darioa@microsoft.com>
2025-08-19 11:52:11 -0700

    version fix

---
+++ src/Diginsight.Components.Configuration.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Diginsight.Components.Configuration.Abstractions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("diginsight")]
+[assembly: AssemblyProduct("Diginsight.Components.Configuration.Abstractions")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1a127a48-04c8-450d-8f9a-513daf941faf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: InternalsVisibleTo("Diginsight.Components, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c55b05f76eed242396626bda7c97a7f0d1214a7678ad48aa9a32156a8e66f5b8df286f1590f98fab58220d0727eb513a87a653bc192687f731dc88970e77c3824ce3d3ce8d6ca5929fcc951c184f9db6c2a95128c726e7cc6dbc59a29c2ddd3898e69641655ad2744e0a31421576c4bd654d779b5388e8a23843af2fb016a3c5")]
+[assembly: InternalsVisibleTo("Diginsight.Components.Abstractions, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c55b05f76eed242396626bda7c97a7f0d1214a7678ad48aa9a32156a8e66f5b8df286f1590f98fab58220d0727eb513a87a653bc192687f731dc88970e77c3824ce3d3ce8d6ca5929fcc951c184f9db6c2a95128c726e7cc6dbc59a29c2ddd3898e69641655ad2744e0a31421576c4bd654d779b5388e8a23843af2fb016a3c5")]
+[assembly: InternalsVisibleTo("Diginsight.Components.Azure, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c55b05f76eed242396626bda7c97a7f0d1214a7678ad48aa9a32156a8e66f5b8df286f1590f98fab58220d0727eb513a87a653bc192687f731dc88970e77c3824ce3d3ce8d6ca5929fcc951c184f9db6c2a95128c726e7cc6dbc59a29c2ddd3898e69641655ad2744e0a31421576c4bd654d779b5388e8a23843af2fb016a3c5")]
+[assembly: InternalsVisibleTo("Diginsight.Components.Azure.Abstractions, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c55b05f76eed242396626bda7c97a7f0d1214a7678ad48aa9a32156a8e66f5b8df286f1590f98fab58220d0727eb513a87a653bc192687f731dc88970e77c3824ce3d3ce8d6ca5929fcc951c184f9db6c2a95128c726e7cc6dbc59a29c2ddd3898e69641655ad2744e0a31421576c4bd654d779b5388e8a23843af2fb016a3c5")]
+[assembly: InternalsVisibleTo("Diginsight.Components.Presentation, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c55b05f76eed242396626bda7c97a7f0d1214a7678ad48aa9a32156a8e66f5b8df286f1590f98fab58220d0727eb513a87a653bc192687f731dc88970e77c3824ce3d3ce8d6ca5929fcc951c184f9db6c2a95128c726e7cc6dbc59a29c2ddd3898e69641655ad2744e0a31421576c4bd654d779b5388e8a23843af2fb016a3c5")]
+[assembly: InternalsVisibleTo("Diginsight.Components.Presentation.Abstractions, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c55b05f76eed242396626bda7c97a7f0d1214a7678ad48aa9a32156a8e66f5b8df286f1590f98fab58220d0727eb513a87a653bc192687f731dc88970e77c3824ce3d3ce8d6ca5929fcc951c184f9db6c2a95128c726e7cc6dbc59a29c2ddd3898e69641655ad2744e0a31421576c4bd654d779b5388e8a23843af2fb016a3c5")]
+// Add other Diginsight.Component assemblies as needed
